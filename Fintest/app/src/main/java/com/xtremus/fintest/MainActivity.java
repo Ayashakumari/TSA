b7d54e056f1d5a1990f5d6e7990756cea02830a9
Nagendra0 <Nagender412001@gmail.com>
2019-06-03 17:37:19 +0530

    device attached

--- Fintest/app/src/main/java/com/xtremus/fintest/MainActivity.java
+++ Fintest/app/src/main/java/com/xtremus/fintest/MainActivity.java
@@ -1,11 +1,15 @@
 package com.xtremus.fintest;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mantra.mfs100.FingerData;
 import com.mantra.mfs100.MFS100;
 import com.mantra.mfs100.MFS100Event;
 
@@ -13,10 +17,62 @@ public class MainActivity extends AppCompatActivity implements MFS100Event {
 
 
     EditText lblMessage;
+    ScannerAction scannerAction = ScannerAction.Capture;
 
 
     int timeout = 10000;
     MFS100 mfs100 = null;
+    private boolean isCaptureRunning = false;
+
+    private void StartSyncCapture() {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                SetTextOnUIThread("");
+                isCaptureRunning = true;
+                try {
+                    FingerData fingerData = new FingerData();
+                    int ret = mfs100.AutoCapture(fingerData, timeout, false);
+                    Log.e("StartSyncCapture.RET", "" + ret);
+                    if (ret != 0) {
+                        SetTextOnUIThread(mfs100.GetErrorMsg(ret));
+                    } else {
+                        lastCapFingerData = fingerData;
+                        final Bitmap bitmap = BitmapFactory.decodeByteArray(fingerData.FingerImage(), 0,
+                                fingerData.FingerImage().length);
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                //imgFinger.setImageBitmap(bitmap);
+                            }
+                        });
+
+                        SetTextOnUIThread("Capture Success");
+                        String log = "\nQuality: " + fingerData.Quality()
+                                + "\nNFIQ: " + fingerData.Nfiq()
+                                + "\nWSQ Compress Ratio: "
+                                + fingerData.WSQCompressRatio()
+                                + "\nImage Dimensions (inch): "
+                                + fingerData.InWidth() + "\" X "
+                                + fingerData.InHeight() + "\""
+                                + "\nImage Area (inch): " + fingerData.InArea()
+                                + "\"" + "\nResolution (dpi/ppi): "
+                                + fingerData.Resolution() + "\nGray Scale: "
+                                + fingerData.GrayScale() + "\nBits Per Pixal: "
+                                + fingerData.Bpp() + "\nWSQ Info: "
+                                + fingerData.WSQInfo();
+                        // SetLogOnUIThread(log);
+                        // SetData2(fingerData);
+                    }
+                } catch (Exception ex) {
+                    SetTextOnUIThread("Error");
+                } finally {
+                    isCaptureRunning = false;
+                }
+            }
+        }).start();
+    }
 
 
     @Override
@@ -42,6 +98,7 @@ public class MainActivity extends AppCompatActivity implements MFS100Event {
             }
         });
     }
+
     private void InitScanner() {
         try {
             int ret = mfs100.Init();
@@ -62,15 +119,21 @@ public class MainActivity extends AppCompatActivity implements MFS100Event {
         }
     }
 
+    public void onLoginClick()
+    {
+        scannerAction = ScannerAction.Capture;
+        if (!isCaptureRunning) {
+            StartSyncCapture();
+        }
+    }
+
     @Override
     public void OnDeviceAttached(int i, int i1, boolean b) {
 
     }
-    public void onClick()
-    {
-        if (!isCaptureRunning) {
-            StartSyncCapture();
-        }
+
+    private enum ScannerAction {
+        Capture, Verify
     }
     @Override
     public void OnDeviceDetached() {
