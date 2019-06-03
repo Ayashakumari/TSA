3406c9fb4cad92345f62759a627b510a4be20158
Xtremus <jayeshmann06@gmail.com>
2019-06-03 13:53:21 +0530

    Added dependencies.

--- Fintest/app/src/main/java/com/xtremus/fintest/MainActivity.java
+++ Fintest/app/src/main/java/com/xtremus/fintest/MainActivity.java
@@ -4,11 +4,30 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.mantra.mfs100.MFS100Event;
+
+
+public class MainActivity extends AppCompatActivity implements MFS100Event {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void OnDeviceAttached(int i, int i1, boolean b) {
+
+    }
+
+    @Override
+    public void OnDeviceDetached() {
+
+    }
+
+    @Override
+    public void OnHostCheckFailed(String s) {
+
     }
 }
