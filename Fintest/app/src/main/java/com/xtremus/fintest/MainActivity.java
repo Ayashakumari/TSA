1f1fbbcdcedff95b3fabeffe9c82354183848fdf
xtremus <jayeshmann06@gmail.com>
2019-06-03 16:06:48 +0530

    Revert "device attached"
    This reverts commit 3042a38d2ba74896c186d358271c95af9fa89c48.

--- Fintest/app/src/main/java/com/xtremus/fintest/MainActivity.java
+++ Fintest/app/src/main/java/com/xtremus/fintest/MainActivity.java
@@ -41,43 +41,9 @@ public class MainActivity extends AppCompatActivity implements MFS100Event {
         });
     }
 
-    private void showSuccessLog(String key) {
-        SetTextOnUIThread("Init success");
-        /*String info = "\nKey: " + key + "\nSerial: "
-                + mfs100.GetDeviceInfo().SerialNo() + " Make: "
-                + mfs100.GetDeviceInfo().Make() + " Model: "
-                + mfs100.GetDeviceInfo().Model()
-                + "\nCertificate: " + mfs100.GetCertification();
-        SetLogOnUIThread(info);*/
-    }
-
 
     @Override
-    public void OnDeviceAttached(int vid, int pid, boolean hasPermission) {
-        int ret;
-        if (!hasPermission) {
-            SetTextOnUIThread("Permission denied");
-            return;
-        }
-        if (vid == 1204 || vid == 11279) {
-            if (pid == 34323) {
-                ret = mfs100.LoadFirmware();
-                if (ret != 0) {
-                    SetTextOnUIThread(mfs100.GetErrorMsg(ret));
-                } else {
-                    SetTextOnUIThread("Load firmware success");
-                }
-            } else if (pid == 4101) {
-                String key = "Without Key";
-                ret = mfs100.Init();
-                if (ret == 0) {
-                    showSuccessLog(key);
-                } else {
-                    SetTextOnUIThread(mfs100.GetErrorMsg(ret));
-                }
-
-            }
-        }
+    public void OnDeviceAttached(int i, int i1, boolean b) {
 
     }
 
